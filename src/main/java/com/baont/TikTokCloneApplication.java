10ff8df495ddddb50a1cecc70cbaeb9bc2273ee4
baovirus <thaibaoo2105@gmail.com>
2025-04-28 01:59:57 +0700

    Set up project
    a

---
+++ src/main/java/com/baont/TikTokCloneApplication.java
@@ -0,0 +1,13 @@
+package com.baont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikTokCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikTokCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baont/TikTokCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikTokCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
